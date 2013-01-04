060d9a3efd609e6f027d56df01c0d5f1f4b7174e
Mantas Mikulėnas <grawity@gmail.com>
2013-01-05 00:09:00 +0200

    Add pure-Tcl base64 decoder

--- g_cap.tcl
+++ g_cap.tcl
@@ -49,6 +49,7 @@ if {![catch {package require Tcl 8.6}]} {
 	set b64map {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 		    a b c d e f g h i j k l m n o p q r s t u v w x y z
 		    0 1 2 3 4 5 6 7 8 9 + / =}
+
 	proc b64:encode {input} {
 		global b64map
 		set str {}
@@ -70,7 +71,40 @@ if {![catch {package require Tcl 8.6}]} {
 		}
 		return $str
 	}
-	#die "No Base64 implementation found; need either Tcl 8.6 or tcllib/base64"
+
+	proc b64:decode {input} {
+		set str {}
+		set pos 0
+		set pad 0
+		set n 0
+		binary scan $input c* X
+		foreach x $X {
+			if     {$x >= 65 && $x <= 90}  { set x [expr {$x - 65}] }\
+			elseif {$x >= 97 && $x <= 122} { set x [expr {$x - 71}] }\
+			elseif {$x >= 48 && $x <= 57}  { set x [expr {$x + 4}]  }\
+			elseif {$x == 61}              { set x 0; incr pad      }\
+			else                           { continue }
+			set o [expr {18 - 6 * ($pos % 4)}]
+			set n [expr {$n | ($x << $o)}]
+			if {$o == 0} {
+				set a [expr {($n >> 16) & 255}]
+				set b [expr {($n >> 8) & 255}]
+				set c [expr {$n & 255}]
+				set n 0
+				if {$pad == 2} {
+					append str [binary format c $a]
+					break
+				} elseif {$pad == 1} {
+					append str [binary format cc $a $b]
+					break
+				} else {
+					append str [binary format ccc $a $b $c]
+				}
+			}
+			incr pos
+		}
+		return $str
+	}
 }
 
 ## Raw events
