e555dc5d7fb9f5c836f90679aca3565b980a729e
Mantas Mikulėnas <grawity@gmail.com>
2019-01-08 23:08:04 +0200

    add SCRAM-SHA1 implementation

---
+++ g_scram.tcl
@@ -0,0 +1,160 @@
+package require Tcl 8.2
+package require sha1
+
+if {[namespace exists ::pbkdf2] == ""} {
+	die "You must load g_pbkdf2.tcl first."
+}
+
+proc scram:mknonce {length} {
+	set fp [open /dev/urandom r]
+	fconfigure $fp -translation binary
+	set buf [read $fp $length]
+	close $fp
+	return [string range [b64:encode $buf] 0 $length]
+}
+
+proc scram:kvparse {string} {
+	set tmp [split $string ","]
+	array set kvps {}
+	foreach word $tmp {
+		if {[regexp {^([A-Za-z])=(.*)$} $word _ k v]} {
+			set kvps($k) $v
+		} else {
+			return
+		}
+	}
+	return [array get kvps]
+}
+
+proc scram:xorbuf {a b} {
+	binary scan $a cu* abuf
+	binary scan $b cu* bbuf
+	set cbuf [lmap ai $abuf bi $bbuf {expr {$ai ^ $bi}}]
+	return [binary format cu* $cbuf]
+}
+
+proc scram:upgrade-config {pass} {
+	global sasl-pass
+	set sasl-pass $pass
+	putlog "You should set sasl-pass to: \"$pass\""
+}
+
+proc sasl:step:SCRAM-SHA-1 {step data} {
+	global sasl-user
+	global sasl-pass
+	global scram-state
+
+	if {$step == 1 && $data == "+"} {
+		set cNonce [scram:mknonce 32]
+		set cInitMsg "n=${sasl-user},r=${cNonce}"
+		array unset scram-state *
+		set scram-state(cNonce) $cNonce
+		set scram-state(cInitMsg) $cInitMsg
+		return [b64:encode "n,,${cInitMsg}"]
+	} elseif {$step == 2 && $data != "+"} {
+		set sFirstMsg [b64:decode $data]
+		array set sKvps [scram:kvparse $sFirstMsg]
+		if {[array get sKvps] == ""} {
+			putlog "ERROR: could not parse SCRAM message '${data}'"
+			return "*"
+		}
+		if {$sKvps(r) == ""} {
+			putlog "ERROR: server nonce missing from SCRAM challenge"
+			return "*"
+		}
+		if {$sKvps(s) == ""} {
+			putlog "ERROR: salt missing from SCRAM challenge"
+			return "*"
+		}
+		if {$sKvps(i) == ""} {
+			putlog "ERROR: iteration count missing from SCRAM challenge"
+			return "*"
+		}
+		set sNonce $sKvps(r)
+		set cNonce ${scram-state(cNonce)}
+		if {[string length $sNonce] <= [string length $cNonce]} {
+			putlog "ERROR: server nonce truncated in SCRAM challenge"
+			return "*"
+		}
+		if {[string range $sNonce 0 [string length $cNonce]-1] != $cNonce} {
+			putlog "ERROR: server/client nonce prefix mismatch in SCRAM challenge"
+			return "*"
+		}
+		# parse password
+		if {[string range ${sasl-pass} 0 5] == "scram:"} {
+			set passTmp [string range ${sasl-pass} 6 [string length ${sasl-pass}]]
+			array set pKvps [scram:kvparse $passTmp] 
+			if {$pKvps(s) != $sKvps(s) || $pKvps(i) != $sKvps(i)} {
+			# TODO: also check algo
+				putlog "ERROR: sasl-pass wrong, salt and/or iteration count mismatch"
+				return "*"
+			}
+			set clientKey [b64:decode $pKvps(C)]
+			set serverKey [b64:decode $pKvps(S)]
+		} else {
+			putlog "SCRAM: sasl-pass is not a stored key, looking for salted hash..."
+			if {[string range ${sasl-pass} 0 6] == "pbkdf2:"} {
+				set passTmp [string range ${sasl-pass} 7 [string length ${sasl-pass}]]
+				array set pKvps [scram:kvparse $passTmp] 
+				if {$pKvps(s) != $sKvps(s) || $pKvps(i) != $sKvps(i)} {
+				# TODO: also check algo
+					putlog "ERROR: sasl-pass wrong, salt and/or iteration count mismatch"
+					return "*"
+				}
+				set saltedPassword [b64:decode $pKvps(h)]
+			} else {
+				set sSalt [b64:decode $sKvps(s)]
+				set sIter $sKvps(i)
+				if {$sSalt == ""} {
+					putlog "ERROR: server provided invalid salt in SCRAM challenge"
+					return "*"
+				}
+				putlog "SCRAM: Plaintext password found in 'sasl-pass'. Calculating PBKDF2 ($sIter iterations)..."
+				if {$sIter > 2000} {
+					putlog "This will take a minute or two. The server will probably kick you off."
+				}
+				set saltedPassword [::pbkdf2::pbkdf2 sha1 ${sasl-pass} $sSalt $sIter]
+				# Tell operator to store the new value
+				set sasl-pass "pbkdf2:s=${sKvps(s)},i=${sKvps(i)},h=[b64:encode $saltedPassword]"
+				scram:upgrade-config ${sasl-pass}
+			}
+			set clientKey [::sha1::hmac -bin -key $saltedPassword -- "Client Key"]
+			set serverKey [::sha1::hmac -bin -key $saltedPassword -- "Server Key"]
+			# Tell operator to store the new value
+			set sasl-pass "scram:s=${sKvps(s)},i=${sKvps(i)},C=[b64:encode $clientKey],S=[b64:encode $serverKey]"
+			scram:upgrade-config ${sasl-pass}
+		}
+		set cInitMsg ${scram-state(cInitMsg)}
+		set cFinalMsgBare "c=biws,r=${sNonce}"
+		set authMsg "$cInitMsg,$sFirstMsg,$cFinalMsgBare"
+		set storedKey [::sha1::sha1 -bin -- $clientKey]
+		set clientSig [::sha1::hmac -bin -key $storedKey -- $authMsg]
+		set serverSig [::sha1::hmac -bin -key $serverKey -- $authMsg]
+		set scram-state(serverSig) $serverSig
+		set clientProof [scram:xorbuf $clientKey $clientSig]
+		set cFinalMsg "$cFinalMsgBare,p=[b64:encode $clientProof]"
+		return [b64:encode $cFinalMsg]
+	} elseif {$step == 3 && $data != "+"} {
+		set sFinalMsg [b64:decode $data]
+		array set sKvps [scram:kvparse $sFinalMsg]
+		if {[array get sKvps] == ""} {
+			putlog "ERROR: could not parse SCRAM message '${data}'"
+			return "*"
+		}
+		if {$sKvps(v) == ""} {
+			putlog "ERROR: server verifier missing from challenge"
+			return "*"
+		}
+		set sVerifier [b64:decode $sKvps(v)]
+		set serverSig ${scram-state(serverSig)}
+		if {$sVerifier != $serverSig} {
+			putlog "ERROR: received server signature does not match computed"
+			return "*"
+		}
+		array unset scram-state *
+		putlog "SCRAM: Server was successfully authenticated."
+		return "+"
+	} else {
+		return "*"
+	}
+}
